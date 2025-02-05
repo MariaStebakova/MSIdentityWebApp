7f637b2f364653d3ca38962f987b6361c069f1e0
Maryia Stsebakova <Maryia_Stsebakova@epam.com>
2025-02-05 12:39:00 +0100

    ASP.NET OAuth Sample

---
+++ OAuthSample/App_Start/Startup.Auth.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.OWIN;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+using Microsoft.IdentityModel.Validators;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System.Configuration;
+
+namespace OAuthSample
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            OwinTokenAcquirerFactory owinTokenAcquirerFactory = TokenAcquirerFactory.GetDefaultInstance<OwinTokenAcquirerFactory>();
+
+            owinTokenAcquirerFactory.Services
+                 .Configure<ConfidentialClientApplicationOptions>(options =>
+                 {
+                     options.RedirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+                     options.ClientSecret = ConfigurationManager.AppSettings["ida:ClientSecret"];
+                     options.ClientId = ConfigurationManager.AppSettings["ida:ClientId"];
+                     options.TenantId = ConfigurationManager.AppSettings["ida:TenantId"];
+                     options.Instance = ConfigurationManager.AppSettings["ida:Instance"];
+                 })
+                 .AddInMemoryTokenCaches();
+
+            owinTokenAcquirerFactory.Build();
+
+            app.AddMicrosoftIdentityWebApp(owinTokenAcquirerFactory,
+                                           updateOptions: options => { });
+        }
+    }
+}
---
+++ OAuthSample/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security;
+using System.Configuration;
+
+namespace OAuthSample.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext()
+                           .Authentication
+                           .Challenge(
+                                new AuthenticationProperties { RedirectUri = "/" },
+                                OpenIdConnectAuthenticationDefaults.AuthenticationType
+                           );
+            }
+        }
+
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext()
+                       .Authentication
+                       .SignOut(
+                            new AuthenticationProperties { RedirectUri = ConfigurationManager.AppSettings["ida:SignedOutCallbackPath"] },
+                            OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                            CookieAuthenticationDefaults.AuthenticationType
+                       );
+        }
+    }
+}
